6292929c4800fcf8f6e8eebd02ca38195e79fac9
Giovani Gonzalez <darkgeat@gmail.com>
2019-03-29 21:54:26 -0600

    Update of the code and repo with the new Firebase library

--- app/src/main/java/training/edu/droidbountyhunter/Home.java
+++ app/src/main/java/training/edu/droidbountyhunter/Home.java
@@ -1,28 +1,25 @@
 package training.edu.droidbountyhunter;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Bundle;
 import android.provider.Settings;
-import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -51,6 +48,7 @@ public class Home extends AppCompatActivity {
      */
     private ViewPager mViewPager;
 
+    @SuppressLint("HardwareIds")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
